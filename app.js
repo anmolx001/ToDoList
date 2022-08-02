796089b51ec65d279763f503d3d6344c90147762
anmolx001 <anmolx001@gmail.com>
2022-08-03 01:35:38 +0530

    @

--- app.js
+++ app.js
@@ -164,6 +164,6 @@ app.get("/about", function(req,res){
 // });
 const port = process.env.PORT || 3000;
 
-app.listen(port, function(){
+app.listen(port, function() {
     console.log("Server has started successfully!");
 });
